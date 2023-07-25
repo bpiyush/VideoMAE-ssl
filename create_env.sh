8194f99564e5839a6fedc0f3554f436e383e5dbe
Piyush Bagad <piyushnbagad11@gmail.com>
2023-07-25 21:31:56 +0200

    WIP: Adds Charades Dataset

--- create_env.sh
+++ create_env.sh
@@ -9,4 +9,4 @@ conda activate videomae
 pip install torch==1.8.0+cu111 torchvision==0.9.0+cu111 torchaudio==0.8.0 -f https://download.pytorch.org/whl/torch_stable.html
 pip install timm==0.4.9
 DS_BUILD_OPS=1 pip install deepspeed
-pip install tensorboardx einops decord
+pip install tensorboardx einops decord jupyterlab ipdb matplotlib opencv-python
